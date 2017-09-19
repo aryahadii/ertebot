dce70bbe6b99e58f01b88e745b3f00c54376f9fb
Arya Hadi <arya@cafebazaar.ir>
2017-09-19 10:38:41 +0430

    Add LastInboxCheckEpoch to model

--- model/person.go
+++ model/person.go
@@ -1,9 +1,10 @@
 package model
 
 type Person struct {
-	UserID       string
-	FirstName    string
-	LastName     string
-	Username     string
-	LastUseEpoch int64
+	UserID              string
+	FirstName           string
+	LastName            string
+	Username            string
+	LastUseEpoch        int64
+	LastInboxCheckEpoch int64
 }
